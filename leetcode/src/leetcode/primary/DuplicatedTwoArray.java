fc89c82b9ad701eb0a772f91c5e68aff071e79f8
jackliao <ldhontheway@live.com>
2019-07-08 22:07:58 +0800

    两个数组的交集 II

--- leetcode/src/leetcode/primary/DuplicatedTwoArray.java
+++ leetcode/src/leetcode/primary/DuplicatedTwoArray.java
@@ -1,59 +1,50 @@
 package leetcode.primary;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * 两个数组的交集 II
- * 给定两个数组，编写一个函数来计算它们的交集
+ * 两个数组的交集 II 给定两个数组，编写一个函数来计算它们的交集
  */
 public class DuplicatedTwoArray {
 
 	public static void main(String[] args) {
-		int[] array1 = { 1, 2, 3, 4, 1,5 };
-		int[] array2 = { 1, 2, 3, 4, 1 };
-		System.out.println(validateDuplicatedArrayItem(array1,array2));
+		int[] array1 = { 1,2,2,1 };
+		int[] array2 = { 2,2 };
+		System.out.println(validateDuplicatedArrayItem(array1,array2)[0]);
+		System.out.println(validateDuplicatedArrayItem(array1,array2)[1]);
 	}
 
-	private static Set<Integer> validateDuplicatedArrayItem(int[] array1,int[] array2) {
-		Set<Integer> set = new TreeSet<>();
+	private static int [] validateDuplicatedArrayItem(int[] array1,int[] array2) {
 		if (array1 == null || array1.length == 0) {
-			return set;
+			return new int[0];
 		}
 		if (array2 == null || array2.length == 0) {
-			return set;
-		}
-
-		for (int i = 0; i < array1.length; i++) {
-			set.add(array1[i]);
+			return new int[0];
 		}
 		
-		Set<Integer> newset = new TreeSet<>();
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < array1.length; i++) {
+			Integer count = map.get(array1[i]);
+			count = (count == null ? 0 : count);
+			map.put(array1[i],++count);
+		}
+		
+		List<Integer> list = new ArrayList<>();
 		for(int i = 0; i < array2.length; i++) {
-			if(set.contains(array2[i])) {
-				newset.add(array2[i]);
-			}
-		}
-
-		return newset;
-	}
-	
-	private static int [] validateDuplicatedArrayItem2(int[] array1,int[] array2) {
-		if (array1 == null || array1.length == 0) {
-			return null;
-		}
-		if (array2 == null || array2.length == 0) {
-			return null;
-		}
-
-		for (int i = 0; i < array1.length; i++) {
-			for(int j = 0; j < array2.length; j++) {
-				
+			if(map.containsKey(array2[i]) && map.get(array2[i]) > 0) {
+				list.add(array2[i]);
+				map.put(array2[i],map.get(array2[i]) - 1);
 			}
 		}
 		
+		int [] array = new int [list.size()];
+		for(int i = 0; i < list.size(); i++) {
+			array[i] = list.get(i);
+		}
 		
-
-		return null;
+		return array;
 	}
 }
