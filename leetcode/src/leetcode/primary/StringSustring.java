731584b7a739bca5b6e6317ef863ef89cd5358a5
jackliao <ldhontheway@live.com>
2019-07-11 07:21:57 +0800

    test substring

---
+++ leetcode/src/leetcode/primary/StringSustring.java
@@ -0,0 +1,10 @@
+package leetcode.primary;
+
+public class StringSustring {
+	public static void main(String[] args) {
+		
+		String s = "\"date/time\",\"settlement id\",\"type\",\"order id\",\"sku\",\"";
+		s = s.replaceAll("\",\"", "&EDA&");
+		System.out.println(s);
+	}
+}
