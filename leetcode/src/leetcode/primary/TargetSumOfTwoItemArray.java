1b55a4274a538f37697ba304065f72b305edda74
jackliao <ldhontheway@live.com>
2019-07-14 10:23:20 +0800

    两数之和 第二个解决方案

--- leetcode/src/leetcode/primary/TargetSumOfTwoItemArray.java
+++ leetcode/src/leetcode/primary/TargetSumOfTwoItemArray.java
@@ -1,5 +1,8 @@
 package leetcode.primary;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 两数之和
  */
@@ -7,8 +10,23 @@ public class TargetSumOfTwoItemArray {
 
 	public static void main(String[] args) {
 		int[] array = { 2, 1, 40, 0, 3, 12 };
-		int[] x = sum(array, 3);
+		int[] x = sum2(array, 3);
 		System.out.println(x[0] +"--"+ x[1]);
+	}
+	
+	public static int[] sum2(int[] nums, int target) {
+		int[] result = new int[2];
+		Map<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			int temp = target - nums[i];
+			if(indexMap.containsKey(temp)) {
+				result[0] = indexMap.get(temp);
+				result[1] = i;
+				return result;
+			}
+			indexMap.put(nums[i],i);
+		}
+		return result;
 	}
 
 	public static int[] sum(int[] nums, int target) {
