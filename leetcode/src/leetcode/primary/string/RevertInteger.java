f1b34ff9e8525999172f9a18a16fb571b62f880c
jackliao <ldhontheway@live.com>
2019-07-20 08:12:26 +0800

    整数反转 第二种解决方案

--- leetcode/src/leetcode/primary/string/RevertInteger.java
+++ leetcode/src/leetcode/primary/string/RevertInteger.java
@@ -8,8 +8,28 @@ public class RevertInteger {
 
 	public static void main(String[] args) {
 		Integer i = -120;
-		i = revert(i);
+		i = revert2(i);
 		System.out.println(i);
+	}
+	/**
+	 * 第二种解决方案 使用取余和除10 乘10
+	 * @param i
+	 * @return
+	 */
+	private static Integer revert2(Integer i) {
+		if(i == null) {
+			return null;
+		}
+		int result = 0;
+		while(i != 0) {
+			int m = i % 10;
+			result = ((result *= 10) + m);
+			i /= 10;
+			if(result > Integer.MAX_VALUE) {
+				return 0;
+			}
+		}
+		return result;
 	}
 
 	private static void revert(char[] strings) {
@@ -46,5 +66,25 @@ public class RevertInteger {
 
 		return l.intValue();
 	}
+	
+	
+	
+	
+	
+	//https://leetcode-cn.com/submissions/detail/23303353/
+	public int reverse(int x) {
+        long result = 0;
+        int max = 0x80000000;
+        int min = 0x7fffffff;
+        while (x != 0) {
+            int mod = x % 10;
+            x = x / 10;
+            result = result * 10 + mod;
+            if (result > Integer.MAX_VALUE || result < (long) Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return (int) result;
+    }
 
 }
