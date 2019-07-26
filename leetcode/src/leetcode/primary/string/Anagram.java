b7d58d0020c55d1b4a89546f22e60d724e8678be
jackliao <ldhontheway@live.com>
2019-07-26 09:44:29 +0800

    有效的字母异位词 第一种解决方案 使用Arrays.sort

--- leetcode/src/leetcode/primary/string/Anagram.java
+++ leetcode/src/leetcode/primary/string/Anagram.java
@@ -1,5 +1,7 @@
 package leetcode.primary.string;
 
+import java.util.Arrays;
+
 /**
  * 有效的字母异位词
  *
@@ -8,10 +10,21 @@ public class Anagram {
 
 	public static void main(String[] args) {
 		String s = "anagram", t = "nagaram";
+		System.out.println(isAnagram(s, t));
 	}
-
+	
+	/**
+	 * 第一种解决方案 使用 Arrays.sort 先排序
+	 * 
+	 * https://leetcode-cn.com/submissions/detail/23970978/
+	 */
 	private static boolean isAnagram(String s, String t) {
-
-		return false;
+		char[] chars0 = s.toCharArray();
+		char[] chars1 = t.toCharArray();
+		Arrays.sort(chars0);
+		Arrays.sort(chars1);
+		s = new String(chars0);
+		t = new String(chars1);
+		return s.equals(t);
 	}
 }
