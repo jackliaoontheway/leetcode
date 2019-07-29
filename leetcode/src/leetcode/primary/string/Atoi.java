40f7cdad71b1e8763d7b6b89079afb3d22cb4191
jackliao <ldhontheway@live.com>
2019-07-29 10:26:21 +0800

    字符串转换整数 (atoi)

---
+++ leetcode/src/leetcode/primary/string/Atoi.java
@@ -0,0 +1,67 @@
+package leetcode.primary.string;
+
+import java.math.BigInteger;
+
+public class Atoi {
+
+	public static void main(String[] args) {
+		System.out.println(myAtoi("2147483648"));
+	}
+
+	public static int myAtoi(String str) {
+
+		if (str == null) {
+			return 0;
+		}
+		str = str.trim();
+		if (str.length() == 0) {
+			return 0;
+		}
+
+		char firstLetter = str.charAt(0);
+
+		if (firstLetter == '-' || firstLetter == '+' || Character.isDigit(firstLetter)) {
+			boolean isNegative = false;
+			if (firstLetter == '-') {
+				isNegative = true;
+				str = str.substring(1);
+			} else if (firstLetter == '+') {
+				str = str.substring(1);
+			}
+			if (str.length() == 0) {
+				return 0;
+			}
+			String numbers = "";
+			for (int i = 0; i < str.length(); i++) {
+				char c = str.charAt(i);
+				if (Character.isDigit(c)) {
+					numbers += c;
+				} else {
+					break;
+				}
+			}
+			if (numbers.length() == 0) {
+				return 0;
+			}
+
+			BigInteger num = new BigInteger(numbers);
+			BigInteger max = new BigInteger(Integer.MAX_VALUE + "");
+			if (num.compareTo(max) > 0) {
+				if (isNegative) {
+					return Integer.MIN_VALUE;
+				}
+				return Integer.MAX_VALUE;
+			}
+
+			if (isNegative) {
+				return num.intValue() * -1;
+			}
+
+			return num.intValue();
+		}
+
+		return 0;
+
+	}
+
+}
