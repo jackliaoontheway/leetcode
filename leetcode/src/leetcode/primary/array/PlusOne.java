f86bae0e08b12517c76d0bf0a0357decd95414b1
jackliao <ldhontheway@live.com>
2019-07-31 17:56:16 +0800

    加一

--- leetcode/src/leetcode/primary/array/PlusOne.java
+++ leetcode/src/leetcode/primary/array/PlusOne.java
@@ -8,22 +8,37 @@ package leetcode.primary.array;
 public class PlusOne {
 
 	public static void main(String[] args) {
-		int[] digits = { 4, 0, 9 };
-		plusOne(digits);
+		int[] digits = { 8, 9, 9, 9 };
+		digits = plusOne2(digits);
 		for (int x : digits) {
 			System.out.println(x);
 		}
 	}
 
-	public static int[] plusOne(int[] digits) {
+	public static int[] plusOne2(int[] digits) {
 		for (int i = digits.length - 1; i >= 0; i--) {
-			digits[i]++;
-			digits[i] = digits[i] % 10;
-			if (digits[i] != 0)
+			digits[i] = (digits[i] + 1) % 10;
+			if (digits[i] != 0) {
 				return digits;
+			}
 		}
 		digits = new int[digits.length + 1];
 		digits[0] = 1;
 		return digits;
 	}
+
+	private static int[] plusOne(int[] digits) {
+		for (int i = digits.length - 1; i >= 0; i--) {
+			digits[i] = digits[i] + 1;
+			if (digits[i] % 10 != 0) {
+				return digits;
+			} else {
+				digits[i] = 0;
+			}
+		}
+		digits = new int[digits.length + 1];
+		digits[0] = 1;
+		return digits;
+	}
+
 }
