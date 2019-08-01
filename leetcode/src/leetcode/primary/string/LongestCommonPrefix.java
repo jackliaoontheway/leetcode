d3b2f904ed58101cce66e2872a25e4b5a8bdefe4
jackliao <ldhontheway@live.com>
2019-08-01 18:38:10 +0800

    最长公共前缀 第二种解决方案

--- leetcode/src/leetcode/primary/string/LongestCommonPrefix.java
+++ leetcode/src/leetcode/primary/string/LongestCommonPrefix.java
@@ -3,10 +3,32 @@ package leetcode.primary.string;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 最长公共前缀
+ */
 public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
 		System.out.println(longestCommonPrefix(new String[] { "f", "f", "" }));
+	}
+
+	public static String longestCommonPrefix2(String[] strs) {
+		if (strs == null || strs.length == 0) {
+			return "";
+		}
+		String pre = strs[0];
+		label: for (int i = 1; i < strs.length; i++) {
+			// 这里的 indexOf 用的很巧妙
+			while (strs[i].indexOf(pre) != 0) {
+				// substring 用的好
+				pre = pre.substring(0, pre.length() - 1);
+				if (pre == "") {
+					break label;
+					// return "";
+				}
+			}
+		}
+		return pre;
 	}
 
 	public static String longestCommonPrefix(String[] strs) {
