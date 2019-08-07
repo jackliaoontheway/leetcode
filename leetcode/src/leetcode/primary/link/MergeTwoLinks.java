2bef8cb7aed6585492f485189f967dc07f0664d0
jackliao <ldhontheway@live.com>
2019-08-07 16:55:50 +0800

    合并两个有序链表 方案二 使用递归

--- leetcode/src/leetcode/primary/link/MergeTwoLinks.java
+++ leetcode/src/leetcode/primary/link/MergeTwoLinks.java
@@ -1,5 +1,8 @@
 package leetcode.primary.link;
 
+/**
+ * 合并两个有序链表
+ */
 public class MergeTwoLinks {
 
 	public static void main(String[] args) {
@@ -27,6 +30,9 @@ public class MergeTwoLinks {
 		}
 	}
 
+	/**
+	 * 方案一
+	 */
 	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 		if (l1 == null) {
 			return l2;
@@ -58,5 +64,22 @@ public class MergeTwoLinks {
 		}
 
 		return header;
+	}
+
+	/**
+	 * 方案二 使用递归
+	 */
+	public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+		if (l1 == null)
+			return l2;
+		if (l2 == null)
+			return l1;
+		if (l1.val < l2.val) {
+			l1.next = mergeTwoLists2(l1.next, l2);
+			return l1;
+		} else {
+			l2.next = mergeTwoLists2(l2.next, l1);
+			return l2;
+		}
 	}
 }
