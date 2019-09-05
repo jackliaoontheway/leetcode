71155b24d0798ffb16e6f32374fdf1c1eb3eae8f
Jack <ldhontheway@live.com>
2019-09-05 14:33:42 +0800

    对角线遍历 第一种解决方案

--- leetcode/src/leetcode/primary/array/FindDiagonalOrder.java
+++ leetcode/src/leetcode/primary/array/FindDiagonalOrder.java
@@ -6,11 +6,61 @@ package leetcode.primary.array;
 public class FindDiagonalOrder {
 
     public static void main(String[] args) {
-
+        int[][] params = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int[] result = findDiagonalOrder(params);
+        for (int x : result) {
+            System.out.print(x);
+        }
     }
 
     public static int[] findDiagonalOrder(int[][] matrix) {
-        return null;
+
+        int times = matrix.length + matrix[0].length - 1;
+        int[] result = new int[matrix.length * matrix[0].length];
+        boolean right = true;
+        int x = 0;
+        int y = 0;
+        int index = 0;
+        for (int i = 0; i < times; i++) {
+            if (right) {
+                while (x >= 0) {
+                    result[index++] = matrix[x][y];
+                    --x;
+                    ++y;
+                    if(y ==  matrix[0].length) {
+                        break;
+                    }
+                }
+                x = 0;
+                if(y ==  matrix[0].length) {
+                    x = 1;
+                    y--;
+                }
+                right = false;
+            } else {
+                while (x < matrix.length) {
+                    result[index++] = matrix[x][y];
+                    if(x == matrix.length - 1) {
+                        y ++;
+                        break;
+                    }
+                    if(y == 0) {
+                        x ++;
+                        break;
+                    }
+                    ++x;
+                    --y;
+                }
+                right = true;
+            }
+        }
+
+
+        return result;
     }
 
 }
