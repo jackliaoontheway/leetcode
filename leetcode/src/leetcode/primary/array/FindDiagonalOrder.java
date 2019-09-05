ea57332bf23bfd8c054c08ef49c70819e0ed361e
Jack <ldhontheway@live.com>
2019-09-05 15:13:28 +0800

    对角线遍历 重构第一种解决方案

--- leetcode/src/leetcode/primary/array/FindDiagonalOrder.java
+++ leetcode/src/leetcode/primary/array/FindDiagonalOrder.java
@@ -11,7 +11,7 @@ public class FindDiagonalOrder {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        int[] result = findDiagonalOrder(params);
+        int[] result = findDiagonalOrder2(params);
         for (int x : result) {
             System.out.print(x);
         }
@@ -31,12 +31,12 @@ public class FindDiagonalOrder {
                     result[index++] = matrix[x][y];
                     --x;
                     ++y;
-                    if(y ==  matrix[0].length) {
+                    if (y == matrix[0].length) {
                         break;
                     }
                 }
                 x = 0;
-                if(y ==  matrix[0].length) {
+                if (y == matrix[0].length) {
                     x = 1;
                     y--;
                 }
@@ -44,12 +44,12 @@ public class FindDiagonalOrder {
             } else {
                 while (x < matrix.length) {
                     result[index++] = matrix[x][y];
-                    if(x == matrix.length - 1) {
-                        y ++;
+                    if (x == matrix.length - 1) {
+                        y++;
                         break;
                     }
-                    if(y == 0) {
-                        x ++;
+                    if (y == 0) {
+                        x++;
                         break;
                     }
                     ++x;
@@ -60,6 +60,49 @@ public class FindDiagonalOrder {
         }
 
 
+        return result;
+    }
+
+    public static int[] findDiagonalOrder2(int[][] matrix) {
+        int times = matrix.length + matrix[0].length - 1;
+        int[] result = new int[matrix.length * matrix[0].length];
+        boolean right = true;
+        int x = 0;
+        int y = 0;
+        int index = 0;
+        for (int i = 0; i < times; i++) {
+            if (right) {
+                while (x > -1) {
+                    result[index++] = matrix[x][y];
+                    if (x == 0 || y == matrix[0].length - 1) {
+                        break;
+                    }
+                    --x;
+                    ++y;
+                }
+                if (y < matrix[0].length - 1) {
+                    y++;
+                } else if (y == matrix[0].length - 1) {
+                    x++;
+                }
+                right = false;
+            } else {
+                while (x < matrix.length) {
+                    result[index++] = matrix[x][y];
+                    if (y == 0 || x == matrix.length - 1) {
+                        break;
+                    }
+                    ++x;
+                    --y;
+                }
+                if (x < matrix.length - 1) {
+                    x++;
+                } else if (x == matrix.length - 1) {
+                    y++;
+                }
+                right = true;
+            }
+        }
         return result;
     }
 
