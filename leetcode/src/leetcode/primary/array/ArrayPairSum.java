d8127dfbf55e7bc2e099bf5f88086a2f9dd62062
Jack <ldhontheway@live.com>
2019-09-11 14:30:01 +0800

    数组拆分I 优化一下for 循环 i += 2

--- leetcode/src/leetcode/primary/array/ArrayPairSum.java
+++ leetcode/src/leetcode/primary/array/ArrayPairSum.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ArrayPairSum {
 
     public static void main(String[] args) {
-        int nums[] = {1,4,3,2};
+        int nums[] = {1, 4, 3, 2};
         System.out.print(arrayPairSum(nums));
     }
 
@@ -15,9 +15,21 @@ public class ArrayPairSum {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i++) {
-            if(i % 2 == 0) {
+            if (i % 2 == 0) {
                 minTotal += nums[i];
             }
+        }
+
+        return minTotal;
+    }
+
+    public static int arrayPairSum2(int[] nums) {
+        int minTotal = 0;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i += 2) {
+            minTotal += nums[i];
         }
 
         return minTotal;
