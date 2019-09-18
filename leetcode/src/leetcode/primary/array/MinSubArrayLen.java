be653238391615bbcb681f5592170350581441af
Jack <ldhontheway@live.com>
2019-09-18 11:04:31 +0800

    长度最小的子数组 第一解法 忽略连续

--- leetcode/src/leetcode/primary/array/MinSubArrayLen.java
+++ leetcode/src/leetcode/primary/array/MinSubArrayLen.java
@@ -1,17 +1,29 @@
 package leetcode.primary.array;
 
+import java.util.Arrays;
+
 /**
  * 长度最小的子数组
  */
 public class MinSubArrayLen {
 
     public static void main(String[] args) {
-
+        int s = 7;
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        System.out.print(minSubArrayLen(7, nums));
     }
 
-    public int minSubArrayLen(int s, int[] nums) {
-
-
+    public static int minSubArrayLen(int s, int[] nums) {
+        Arrays.sort(nums);
+        int l = 0;
+        int sum = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            sum += nums[i];
+            l++;
+            if (sum >= s) {
+                return l;
+            }
+        }
         return 0;
     }
 
