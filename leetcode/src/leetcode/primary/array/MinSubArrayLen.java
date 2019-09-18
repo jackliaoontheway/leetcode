57b242dcdce9c7cc9e67ddb0dcb5632e3ca4d1ae
Jack <ldhontheway@live.com>
2019-09-18 18:16:02 +0800

    长度最小的子数组 第二解法 效率低 https://leetcode-cn.com/submissions/detail/30279887/

--- leetcode/src/leetcode/primary/array/MinSubArrayLen.java
+++ leetcode/src/leetcode/primary/array/MinSubArrayLen.java
@@ -4,20 +4,26 @@ import java.util.Arrays;
 
 /**
  * 长度最小的子数组
+ *
+ * https://leetcode-cn.com/submissions/detail/30279887/
  */
 public class MinSubArrayLen {
 
     public static void main(String[] args) {
-        int s = 7;
-        int[] nums = {2, 3, 1, 2, 3, 3, 2};
-        System.out.print(minSubArrayLen2(7, nums));
+        int s = 15;
+        int[] nums = {5, 1, 3, 5, 10, 7, 4, 9, 2, 8};
+
+        System.out.print(minSubArrayLen3(15, nums));
     }
 
     /**
      * 第二种解法
      */
     public static int minSubArrayLen3(int s, int[] nums) {
-        int minLen = 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int minLen = Integer.MAX_VALUE;
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] >= s) {
@@ -25,18 +31,17 @@ public class MinSubArrayLen {
             }
             sum += nums[i];
             if (sum >= s) {
-                int x = 1;
-                int temp = nums[i];
-                for (int j = i - 1; j >= 0; j--) {
-                    x++;
-                    temp += nums[j];
-                    if (temp >= s) {
-                        minLen = minLen == 0 ? x : Math.min(minLen, x);
+                int temp = sum;
+                for (int j = 0; j < i; j++) {
+                    temp -= nums[j];
+                    if (temp <= s) {
+                        minLen = Math.min(minLen, i - j + 1);
+                        break;
                     }
                 }
             }
         }
-        return minLen;
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
 
     /**
