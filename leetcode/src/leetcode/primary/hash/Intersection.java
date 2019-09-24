fb8534e0f6defd7d32b30c04db5c36425a0111be
Jack <ldhontheway@live.com>
2019-09-24 09:41:44 +0800

    两个数组的交集 template

---
+++ leetcode/src/leetcode/primary/hash/Intersection.java
@@ -0,0 +1,20 @@
+package leetcode.primary.hash;
+
+/**
+ * 两个数组的交集
+ */
+public class Intersection {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int [] result = null;
+
+        return result;
+    }
+
+
+}
