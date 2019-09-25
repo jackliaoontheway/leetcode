9a9eeb4989528bff53cf04dc3868fc053514dcb6
Jack <ldhontheway@live.com>
2019-09-25 09:36:45 +0800

    快乐数 template

---
+++ leetcode/src/leetcode/primary/hash/HappyNumber.java
@@ -0,0 +1,16 @@
+package leetcode.primary.hash;
+
+/**
+ * 快乐数
+ */
+public class HappyNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isHappy(int n) {
+
+        return false;
+    }
+}
