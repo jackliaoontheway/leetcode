e50755d67b8f1e56894da14adce3f64a0da7e103
Jack <ldhontheway@live.com>
2019-09-25 18:34:15 +0800

    快乐数 Implementation

--- leetcode/src/leetcode/primary/hash/HappyNumber.java
+++ leetcode/src/leetcode/primary/hash/HappyNumber.java
@@ -1,16 +1,35 @@
 package leetcode.primary.hash;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 快乐数
  */
 public class HappyNumber {
 
     public static void main(String[] args) {
-
+        System.out.print(new HappyNumber().isHappy(2));
     }
 
-    public boolean isHappy(int n) {
+    Set<Integer> set = new HashSet<>();
 
-        return false;
+    public boolean isHappy(int n) {
+        if (n == 1) {
+            return true;
+        }
+        if(set.contains(n)) {
+            return false;
+        }
+        set.add(n);
+        return isHappy(calculate(n));
+    }
+
+    private int calculate(int n) {
+        if (n < 10) {
+            return n * n;
+        }
+        int m = n % 10;
+        return calculate(n / 10) + (m * m);
     }
 }
