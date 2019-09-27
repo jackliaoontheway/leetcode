232f04c770420a4eb1fc4b85124c97d5c26ad741
Jack <ldhontheway@live.com>
2019-09-27 18:02:52 +0800

    同构字符串 第二种解决方案

--- leetcode/src/leetcode/primary/hash/Isomorphic.java
+++ leetcode/src/leetcode/primary/hash/Isomorphic.java
@@ -5,15 +5,17 @@ import java.util.Map;
 
 /**
  * 同构字符串
+ * https://leetcode-cn.com/submissions/detail/31435224/
  */
 public class Isomorphic {
 
     public static void main(String[] args) {
-        System.out.print(new Isomorphic().isIsomorphic("",""));
+        System.out.print(new Isomorphic().isIsomorphic2("abcd", "aaaa"));
     }
 
     /**
      * 解决方案一
+     *
      * @param s
      * @param t
      * @return
@@ -22,7 +24,7 @@ public class Isomorphic {
         if (s == null || t == null || s.length() != t.length()) {
             return false;
         }
-        if(s.equals(t)) {
+        if (s.equals(t)) {
             return true;
         }
         Map<String, String> map0 = new HashMap<>();
@@ -38,5 +40,35 @@ public class Isomorphic {
             result1 += map1.get(t.substring(i, i + 1));
         }
         return result0.equals(t) && result1.equals(s);
+    }
+
+    /**
+     * 解决方案二
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isIsomorphic2(String s, String t) {
+        if (s == null || t == null || s.length() != t.length()) {
+            return false;
+        }
+        if (s.equals(t)) {
+            return true;
+        }
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.substring(i, i + 1)) == null) {
+                if(map.containsValue(t.substring(i, i + 1))) {
+                    return false;
+                }
+                map.put(s.substring(i, i + 1), t.substring(i, i + 1));
+            } else {
+                if (!map.get(s.substring(i, i + 1)).equals(t.substring(i, i + 1))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
