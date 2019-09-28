e550023140b42d0c6dbf980a1faef34bd5c93ec6
Jack <ldhontheway@live.com>
2019-09-28 10:34:43 +0800

    存在重复元素 II

---
+++ leetcode/src/leetcode/primary/hash/NearbyDuplicate.java
@@ -0,0 +1,15 @@
+package leetcode.primary.hash;
+
+/**
+ * 存在重复元素 II
+ */
+public class NearbyDuplicate {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+    }
+}
