9655ee8a4b1f026801cc8469e9ae28f5c839d9f3
Jack <ldhontheway@live.com>
2019-10-03 22:17:23 +0800

    无重复字符的最长子串 template

---
+++ leetcode/src/leetcode/primary/hash/LongestSubstring.java
@@ -0,0 +1,17 @@
+package leetcode.primary.hash;
+
+/**
+ * 无重复字符的最长子串
+ */
+public class LongestSubstring {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+
+        return -1;
+    }
+
+}
