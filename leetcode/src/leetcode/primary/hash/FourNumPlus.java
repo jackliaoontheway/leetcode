6a99665926bb3d23ec12c4142f29b99d0465dc9c
Jack <ldhontheway@live.com>
2019-10-05 21:48:08 +0800

    四数相加 II template

---
+++ leetcode/src/leetcode/primary/hash/FourNumPlus.java
@@ -0,0 +1,15 @@
+package leetcode.primary.hash;
+
+/**
+ * 四数相加 II
+ */
+public class FourNumPlus {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+
+    }
+}
