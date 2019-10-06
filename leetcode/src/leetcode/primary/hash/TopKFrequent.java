5f4373b8618a566ed5b7827cd7df8a62fe68c615
Jack <ldhontheway@live.com>
2019-10-06 21:49:17 +0800

    前 K 个高频元素 template

---
+++ leetcode/src/leetcode/primary/hash/TopKFrequent.java
@@ -0,0 +1,19 @@
+package leetcode.primary.hash;
+
+import java.util.List;
+
+/**
+ * 前 K 个高频元素
+ */
+public class TopKFrequent {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        return o;
+    }
+}
