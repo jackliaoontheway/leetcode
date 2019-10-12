345079cf14079aa92780112d251e1fa53e0533c4
Jack <ldhontheway@live.com>
2019-10-12 11:31:12 +0800

    前 K 个高频元素 implementation

--- leetcode/src/leetcode/primary/hash/TopKFrequent.java
+++ leetcode/src/leetcode/primary/hash/TopKFrequent.java
@@ -1,6 +1,6 @@
 package leetcode.primary.hash;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 前 K 个高频元素
@@ -9,15 +9,40 @@ public class TopKFrequent {
 
     public static void main(String[] args) {
 
-        int[] nums  = {};
-        int k = 1;
-        List<Integer> result = new TopKFrequent().topKFrequent(nums,k);
-
+        int[] nums = {1,2};
+        int k = 2;
+        List<Integer> result = new TopKFrequent().topKFrequent(nums, k);
+        for (int x : result) {
+            System.out.print(x);
+        }
     }
 
 
     public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
 
-        return null;
+        // 放元素出现的次数Map key = 元素 value 是次数
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            int times = 0;
+            if (map.containsKey(num)) {
+                times = map.get(num);
+            }
+            times++;
+            map.put(num, times);
+        }
+
+        List<Integer> list = new ArrayList(map.values());
+        Collections.sort(list);
+        int x = list.get(list.size() - k);
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() >= x) {
+                result.add(entry.getKey());
+            }
+        }
+        return result;
     }
 }
