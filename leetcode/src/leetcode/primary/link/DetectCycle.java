42a638283cd1771a07db1d5684bae4541befdba5
Jack <ldhontheway@live.com>
2019-10-15 11:32:09 +0800

    环形链表 II Template

---
+++ leetcode/src/leetcode/primary/link/DetectCycle.java
@@ -0,0 +1,16 @@
+package leetcode.primary.link;
+
+/**
+ * 环形链表 II
+ */
+public class DetectCycle {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode detectCycle(ListNode head) {
+
+        return null;
+    }
+}
