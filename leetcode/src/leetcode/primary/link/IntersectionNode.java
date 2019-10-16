86e414ba26c8629088aaf13c92dd7f81aa1e5350
Jack <ldhontheway@live.com>
2019-10-16 11:42:01 +0800

    相交链表 template

---
+++ leetcode/src/leetcode/primary/link/IntersectionNode.java
@@ -0,0 +1,17 @@
+package leetcode.primary.link;
+
+/**
+ * 相交链表
+ */
+public class IntersectionNode {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        return null;
+    }
+
+}
