96e5787e91d764463de040fa1a0d7e9c146f520a
Jack <ldhontheway@live.com>
2019-10-19 14:58:35 +0800

    奇偶链表 template

---
+++ leetcode/src/leetcode/primary/link/OddEvenList.java
@@ -0,0 +1,16 @@
+package leetcode.primary.link;
+
+/**
+ * 奇偶链表
+ */
+public class OddEvenList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+
+        return head;
+    }
+}
