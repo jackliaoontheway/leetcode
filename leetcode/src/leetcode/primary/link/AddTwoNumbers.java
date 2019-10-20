3db66181efdebf88a0fad361b923961c633c0e0a
Jack <ldhontheway@live.com>
2019-10-21 00:17:18 +0800

    两数相加 template

---
+++ leetcode/src/leetcode/primary/link/AddTwoNumbers.java
@@ -0,0 +1,16 @@
+package leetcode.primary.link;
+
+/**
+ * 两数相加
+ */
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+}
