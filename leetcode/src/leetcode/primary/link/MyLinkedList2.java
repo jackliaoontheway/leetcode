085a879258a25807f77ce09b17216cc00c520276
Jack <ldhontheway@live.com>
2019-10-22 21:21:33 +0800

    MyLinkedList2 template

---
+++ leetcode/src/leetcode/primary/link/MyLinkedList2.java
@@ -0,0 +1,36 @@
+package leetcode.primary.link;
+
+/**
+ * 设计链表
+ */
+public class MyLinkedList2 {
+    /** Initialize your data structure here. */
+    public MyLinkedList() {
+
+    }
+
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int get(int index) {
+
+    }
+
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+
+    }
+
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+
+    }
+
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+
+    }
+
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+
+    }
+}
