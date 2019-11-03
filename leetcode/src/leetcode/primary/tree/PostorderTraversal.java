5946b037c7fc46f4a9cdeb2fed72f574a0e07397
Jack <ldhontheway@live.com>
2019-11-03 09:48:26 +0800

    二叉树的后序遍历 第一种解决方案 递归

---
+++ leetcode/src/leetcode/primary/tree/PostorderTraversal.java
@@ -0,0 +1,45 @@
+package leetcode.primary.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树的后序遍历
+ */
+public class PostorderTraversal {
+
+    public static void main(String[] args) {
+
+        TreeNode treeNode = TreeNode.getData();
+
+        List<Integer> result = new PostorderTraversal().postorderTraversal(treeNode);
+
+        for (Integer item : result) {
+            System.out.print(item + " ");
+        }
+
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList();
+
+        iterator(result, root);
+
+        return result;
+    }
+
+    private void iterator(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.right != null) {
+            iterator(result, root.right);
+        }
+        if (root.left != null) {
+            iterator(result, root.left);
+        }
+        result.add(root.val);
+    }
+}
