f703fb5f2ff3723712c34d4e9e04461181cab2a3
Jack <ldhontheway@live.com>
2019-11-04 16:31:36 +0800

    二叉树的前序遍历 第二种解决方案 使用循环

--- leetcode/src/leetcode/primary/tree/PreorderTraversal.java
+++ leetcode/src/leetcode/primary/tree/PreorderTraversal.java
@@ -1,7 +1,9 @@
 package leetcode.primary.tree;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PreorderTraversal {
 
@@ -9,7 +11,8 @@ public class PreorderTraversal {
 
         TreeNode treeNode = TreeNode.getData();
 
-        List<Integer> result = new PreorderTraversal().preorderTraversal(treeNode);
+        List<Integer> result = new PreorderTraversal().preorderTraversal2(treeNode);
+        //List<Integer> result = new PreorderTraversal().preorderTraversal(treeNode);
 
         for (Integer item : result) {
             System.out.print(item + " ");
@@ -38,4 +41,45 @@ public class PreorderTraversal {
             iterator(result, root.right);
         }
     }
+
+
+    /**
+     * 第二种解决方案不用递归
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> result = new ArrayList();
+        if (root == null) {
+            return result;
+        }
+
+        List<TreeNode> temp = new ArrayList<>();
+        Set<TreeNode> set = new HashSet<>();
+        while (true) {
+            if (!set.contains(root)) {
+                result.add(root.val);
+                set.add(root);
+            }
+            if (root.left != null && !set.contains(root.left)) {
+                temp.add(root);
+                root = root.left;
+                continue;
+            }
+            if (root.right != null) {
+                root = root.right;
+                continue;
+            }
+            if (!temp.isEmpty()) {
+                root = temp.get(temp.size() - 1);
+                temp.remove(temp.size() - 1);
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
+
+
 }
--- leetcode/src/leetcode/primary/tree/TreeNode.java
+++ leetcode/src/leetcode/primary/tree/TreeNode.java
@@ -13,13 +13,13 @@ public class TreeNode {
 
     public static TreeNode getData() {
 
-        TreeNode treeNode0 = new TreeNode(0);
-        TreeNode treeNode1 = new TreeNode(1);
-        TreeNode treeNode2 = new TreeNode(2);
-        TreeNode treeNode3 = new TreeNode(3);
-        TreeNode treeNode4 = new TreeNode(4);
-        TreeNode treeNode5 = new TreeNode(5);
-        TreeNode treeNode6 = new TreeNode(6);
+        TreeNode treeNode0 = new TreeNode(1);
+        TreeNode treeNode1 = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(3);
+        TreeNode treeNode3 = new TreeNode(4);
+        TreeNode treeNode4 = new TreeNode(5);
+        TreeNode treeNode5 = new TreeNode(6);
+        TreeNode treeNode6 = new TreeNode(7);
 
         treeNode0.left = treeNode1;
         treeNode0.right = treeNode2;
