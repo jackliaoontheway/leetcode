70e889a6a4f278bfd6e02104a207b94d9ba6df7a
Jack <ldhontheway@live.com>
2019-11-11 18:06:21 +0800

    二叉树的最大深度

--- leetcode/src/leetcode/primary/tree/MaxDepth.java
+++ leetcode/src/leetcode/primary/tree/MaxDepth.java
@@ -1,20 +1,36 @@
 package leetcode.primary.tree;
 
+/**
+ * 二叉树的最大深度
+ */
 public class MaxDepth {
-
 
     public static void main(String[] args) {
 
         TreeNode treeNode = TreeNode.getData();
 
-
-
+        System.out.println(new MaxDepth().maxDepth(treeNode));
     }
 
-    private  static int maxDepth(TreeNode treeNode) {
+    private int maxDepth(TreeNode treeNode) {
+        maxDepth(treeNode, 0);
+        return maxDepth;
+    }
 
+    int maxDepth;
 
-        return 1;
+    private void maxDepth(TreeNode treeNode, int depth) {
+        if (treeNode == null) {
+            return;
+        }
+
+        if (treeNode.left == null && treeNode.right == null) {
+            maxDepth = Math.max(maxDepth, depth);
+        }
+
+        maxDepth(treeNode.left, depth + 1);
+        maxDepth(treeNode.right, depth + 1);
+
     }
 
 }
