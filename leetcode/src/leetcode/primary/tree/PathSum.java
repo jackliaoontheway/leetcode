744636bb6fe8b20fa349a1b8715537995c3fce49
Jack <ldhontheway@live.com>
2019-11-16 21:52:58 +0800

    路径总和 template

---
+++ leetcode/src/leetcode/primary/tree/PathSum.java
@@ -0,0 +1,17 @@
+package leetcode.primary.tree;
+
+/**
+ * 路径总和
+ */
+public class PathSum {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+
+        return false;
+    }
+}
