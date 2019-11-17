2e870ea56cde56d43d1d555ade363abc216c0c65
Jack <ldhontheway@live.com>
2019-11-17 15:30:04 +0800

    二叉树的层次遍历 queu 实现

--- leetcode/src/leetcode/primary/tree/LevelOrder.java
+++ leetcode/src/leetcode/primary/tree/LevelOrder.java
@@ -10,7 +10,7 @@ public class LevelOrder {
     public static void main(String[] args) {
         TreeNode treeNode = TreeNode.getData();
 
-        List<List<Integer>> result = new LevelOrder().levelOrder(treeNode);
+        List<List<Integer>> result = new LevelOrder().levelOrder2(treeNode);
 
         for (List<Integer> items : result) {
             System.out.println();
@@ -21,6 +21,11 @@ public class LevelOrder {
     }
 
 
+    /**
+     * stack 实现
+     * @param root
+     * @return
+     */
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) {
@@ -44,6 +49,35 @@ public class LevelOrder {
             }
             for (int i = 0, len = tempStack.size(); i < len; i++) {
                 stack.add(tempStack.pop());
+            }
+        }
+        return result;
+    }
+
+    /**
+     * queu 实现
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> nodeList = new ArrayList<>();
+            result.add(nodeList);
+            for (int i = 0, len = queue.size(); i < len; i++) {
+                TreeNode node = queue.poll();
+                nodeList.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
             }
         }
         return result;
