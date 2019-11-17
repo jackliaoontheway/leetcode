b4d6e2837332e77f8cf7ca4b346f667132923872
Jack <ldhontheway@live.com>
2019-11-17 15:21:13 +0800

    二叉树的层次遍历 stack 实现

--- leetcode/src/leetcode/primary/tree/LevelOrder.java
+++ leetcode/src/leetcode/primary/tree/LevelOrder.java
@@ -1,8 +1,6 @@
 package leetcode.primary.tree;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * 二叉树的层次遍历
@@ -15,6 +13,7 @@ public class LevelOrder {
         List<List<Integer>> result = new LevelOrder().levelOrder(treeNode);
 
         for (List<Integer> items : result) {
+            System.out.println();
             for (Integer item : items) {
                 System.out.print(item + " ");
             }
@@ -24,30 +23,29 @@ public class LevelOrder {
 
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-
-        Stack<TreeNode> stack = new Stack<>();
-
-        List<Integer> firstLevelNodeList = new ArrayList<>();
-        firstLevelNodeList.add(root.val);
-        result.add(firstLevelNodeList);
-
-        stack.push(root);
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.add(root);
         while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            List<Integer> levelNodeList = new ArrayList<>();
-            if (node.left != null) {
-                stack.push(node.left);
-                levelNodeList.add(node.left.val);
+            List<Integer> firstLevelNodeList = new ArrayList<>();
+            result.add(firstLevelNodeList);
+            Stack<TreeNode> tempStack = new Stack<TreeNode>();
+            for (int i = 0, len = stack.size(); i < len; i++) {
+                TreeNode node = stack.pop();
+                firstLevelNodeList.add(node.val);
+                if (node.left != null) {
+                    tempStack.add(node.left);
+                }
+                if (node.right != null) {
+                    tempStack.add(node.right);
+                }
             }
-            if (node.right != null) {
-                stack.push(node.right);
-                levelNodeList.add(node.right.val);
-            }
-            if(levelNodeList.size() != 0) {
-                result.add(levelNodeList);
+            for (int i = 0, len = tempStack.size(); i < len; i++) {
+                stack.add(tempStack.pop());
             }
         }
-
         return result;
     }
 }
