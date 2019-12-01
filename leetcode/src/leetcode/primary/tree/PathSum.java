ab4751e68618f87da00597cd24edef948b4326b3
Jack <ldhontheway@live.com>
2019-12-01 15:44:00 +0800

    路径总和 第一种解决方案 使用递归

--- leetcode/src/leetcode/primary/tree/PathSum.java
+++ leetcode/src/leetcode/primary/tree/PathSum.java
@@ -1,17 +1,63 @@
 package leetcode.primary.tree;
 
+import java.util.Stack;
+
 /**
- * 路径总和
+ * 路径总和 第一种解决方案 使用递归
  */
 public class PathSum {
 
     public static void main(String[] args) {
+        TreeNode treeNode = TreeNode.getData();
+
+        System.out.print(new PathSum().hasPathSum(treeNode, 5));
 
     }
 
-
+    /**
+     * 第一种解决方案 使用递归
+     * @param root
+     * @param sum
+     * @return
+     */
     public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
 
+        if (root.left == null && root.right == null) {
+            return sum - root.val == 0;
+        }
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+
+    public boolean hasPathSum2(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        int count = 0;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.add(current);
+                if (current.left == null && current.right == null) {
+                    if ((count + current.val) == sum) {
+                        return true;
+                    }
+                } else {
+                    if ((count + current.val) > sum) {
+                        break;
+                    }
+                    count += current.val;
+                }
+                current = current.left;
+            }
+            current = stack.pop();
+            current = current.right;
+        }
         return false;
     }
 }
--- leetcode/src/leetcode/primary/tree/TreeNode.java
+++ leetcode/src/leetcode/primary/tree/TreeNode.java
@@ -15,11 +15,11 @@ public class TreeNode {
 
         TreeNode treeNode0 = new TreeNode(1);
         TreeNode treeNode1 = new TreeNode(2);
-        TreeNode treeNode2 = new TreeNode(2);
-        TreeNode treeNode3 = new TreeNode(3);
-        TreeNode treeNode4 = new TreeNode(4);
-        TreeNode treeNode5 = new TreeNode(4);
-        TreeNode treeNode6 = new TreeNode(3);
+        TreeNode treeNode2 = new TreeNode(3);
+        TreeNode treeNode3 = new TreeNode(4);
+        TreeNode treeNode4 = new TreeNode(5);
+        TreeNode treeNode5 = new TreeNode(6);
+        TreeNode treeNode6 = new TreeNode(7);
 
         treeNode0.left = treeNode1;
         treeNode0.right = treeNode2;
